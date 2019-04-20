5ae368dc85a807a0587b2801abeae9d4605995d8
Himanshu Agarwal <himanshu.agarwal@nagarro.com>
2019-04-21 03:10:34 +0530

    filters added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 
-import {MatPaginator, MatTableDataSource, MatDialog, MatDialogRef, MatSnackBar} from '@angular/material';
+import {MatPaginator, MatTableDataSource, MatDialog, MatDialogRef, MatSnackBar, MatSort} from '@angular/material';
 
 import { StudentService } from '../services/student.service';
 import { Student } from '../models/student';
@@ -26,8 +26,15 @@ export class DashboardComponent implements OnInit {
   ];
   dataSource;
   dialogRef: MatDialogRef<ConfirmationComponent>;
+  category = '';
+  categories = [
+    {value: '', viewValue: 'All'},
+    {value: 'domestic', viewValue: 'Domestic'},
+    {value: 'international', viewValue: 'International'}
+  ];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private studentService: StudentService,
               private router: Router,
@@ -38,8 +45,29 @@ export class DashboardComponent implements OnInit {
     this.studentService.getStudents().subscribe((result: Student[]) => {
       this.students = result;
       this.dataSource = new MatTableDataSource<Student>(this.students);
+      this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
     });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource = new MatTableDataSource<Student>(this.students.filter((student) => {
+      return student.name.toLowerCase().includes(filterValue.trim().toLowerCase());
+    }));
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onCategorySelection() {
+    if (this.category) {
+      this.dataSource = new MatTableDataSource<Student>(this.students.filter((student) => {
+        return student.category === this.category;
+      }));
+    } else {
+      this.dataSource = new MatTableDataSource<Student>(this.students);
+    }
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   viewStudent(id) {
@@ -66,6 +94,7 @@ export class DashboardComponent implements OnInit {
           this.snackBar.open('Student deleted successfully');
           this.students = data;
           this.dataSource = new MatTableDataSource<Student>(this.students);
+          this.dataSource.sort = this.sort;
           this.dataSource.paginator = this.paginator;
         });
       }
